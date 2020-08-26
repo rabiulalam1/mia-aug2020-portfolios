9edcb2b882936c3d5b8e78b23435ea7db0eb8cf4
rabiulalam1 <rabiul.alam@outlook.com>
2020-08-26 10:45:14 -0400

    V1.0 Complete

--- javascript.js
+++ javascript.js
@@ -15,60 +15,61 @@ function submitFunction() {
     });
 }
 
+function remove(e) {
+  console.log("hello", e);
+  let deleteItem = e.target.parentElement.parentElement;
+  let deleteName = deleteItem.querySelector(".first-name span").innerHTML;
+
+  axios
+    .delete(
+      `https://ironrest.herokuapp.com/deleteOne/mia-aug2020-webdevs?first_name=` +
+        deleteName
+    )
+    .then((res) => {
+      location.reload();
+    });
+}
+
 function createTable() {
-  axios.get(`https://ironrest.herokuapp.com/mia-aug2020-webdevs`).then((res) => {
-    for (let i of res.data) {
-      document.querySelector("#tableBody").innerHTML +=
-        `<tr class="portfolios">
-            <td class="first-name">
-              <span>` +
-        i.first_name +
-        `</span>
-            </td>
-            <td class="last-name"><span>` +
-        i.last_name +
-        `</span></td>
-            <td class="link">
-              <span><a href="` +
-        i.website +
-        `">` +
-        i.first_name +
-        `'s Portfolio</a></span>
-            </td>
-            <td class="action">
-              <button class="btn btn-remove" onclick="removeFunction()">Remove</button>
-            </td>
-          </tr>`;
-    }
-  });
+  axios
+    .get(`https://ironrest.herokuapp.com/mia-aug2020-webdevs`)
+    .then((res) => {
+      for (let i of res.data) {
+        let row = document.createElement('tr')
+        row.classList.add('portfolios')
+        row.innerHTML = `
+        <td class="first-name">
+          <span>` +
+      i.first_name +
+      `</span>
+        </td>
+        <td class="last-name"><span>` +
+      i.last_name +
+      `</span></td>
+        <td class="link">
+          <span><a href="` +
+      i.website +
+      `">` +
+      i.first_name +
+      `'s Portfolio</a></span>
+        </td>
+        <td class="action">
+          <button class="btn btn-remove">Remove</button>
+        </td>` 
+
+        row.querySelector('.btn-remove').onclick = remove;
+        document.querySelector("#tableBody").appendChild(row);
+    
+      }
+    });
 }
 
 createTable();
 
-function removeFunction() {
-  let removeButtons = document.querySelectorAll(".btn-remove");
-
-  for (let i = 0; i < removeButtons.length; i++) {
-    let eachButton = removeButtons[i];
-    eachButton.onclick = function deleteData(e) {
-      let deleteItem = e.target.parentElement.parentElement;
-      let deleteName = deleteItem.querySelector(".first-name span").innerHTML;
-
-      axios
-        .delete(
-          `https://ironrest.herokuapp.com/deleteOne/mia-aug2020-webdevs?first_name=` +
-            deleteName
-        )
-        .then((res) => {
-          location.reload();
-        });
-    };
-  }
-}
-
+// ****This codesnippet is to get data from database****
 // axios
-//         .get(`https://ironrest.herokuapp.com/antguts`)
+//         .get(`https://ironrest.herokuapp.com/mia-aug2020-webdevs`)
 //         .then((res)=>{
-//           for (let i=0; i<5 ; i++)
-//             console.log(res.data[i].first_name)
+//           for (let i of res.data)
+//             console.log(i.first_name)
 //         })
