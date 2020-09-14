8e8b13bc394710028e478c2156445c30ee069b63
Rabiul Alam <rabiul.alam@outlook.com>
2020-09-14 02:46:14 -0400

    Added Game Project Section

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function submitFunction() {
+function portfolioSubmit() {
   let firstName = document.querySelector("#first-name-input").value;
   let lasttName = document.querySelector("#last-name-input").value;
   let url = document.querySelector("#url").value;
@@ -15,6 +15,23 @@ function submitFunction() {
     });
 }
 
+function gameSubmit() {
+  let teamName = document.querySelector("#team-name-input").value;
+  let gameName = document.querySelector("#game-name-input").value;
+  let gUrl = document.querySelector("#gUrl").value;
+
+  axios
+    .post(`https://ironrest.herokuapp.com/mia-aug2020-webdevs-games`, {
+      team_name: teamName,
+      game_name: gameName,
+      website: gUrl,
+    })
+    .then((res) => {
+      createGameTable();
+      location.reload();
+    });
+}
+
 function remove(e) {
   let deleteItem = e.target.parentElement.parentElement;
   let deleteName = deleteItem.querySelector(".first-name span").innerHTML;
@@ -22,6 +39,20 @@ function remove(e) {
   axios
     .delete(
       `https://ironrest.herokuapp.com/deleteOne/mia-aug2020-webdevs?first_name=` +
+        deleteName
+    )
+    .then((res) => {
+      location.reload();
+    });
+}
+
+function gameRemove(e) {
+  let deleteItem = e.target.parentElement.parentElement;
+  let deleteName = deleteItem.querySelector(".team-name span").innerHTML;
+
+  axios
+    .delete(
+      `https://ironrest.herokuapp.com/deleteOne/mia-aug2020-webdevs-games?team_name=` +
         deleteName
     )
     .then((res) => {
@@ -63,12 +94,52 @@ function createTable() {
 
         row.querySelector(".btn-remove").onclick = remove;
         row.querySelector(".btn-edit").onclick = edit;
-        document.querySelector("#tableBody").appendChild(row);
+        document.querySelector("#tablePortfolio").appendChild(row);
+      }
+    });
+}
+
+function createGameTable() {
+  axios
+    .get(`https://ironrest.herokuapp.com/mia-aug2020-webdevs-games`)
+    .then((res) => {
+      for (let i of res.data) {
+        let row = document.createElement("tr");
+        row.classList.add("game");
+        row.innerHTML =
+          `<td class="team-name">
+                          <span id=` +
+          i._id +
+          `>` +
+          i.team_name +
+          `</span>
+                        </td>
+                        <td class="game-name">
+                          <span>` +
+          i.game_name +
+          `</span>
+                        </td>
+                        <td class="gameLink">
+                          <span><a href="` +
+          i.website +
+          `">` +
+          i.game_name +
+          `'s Website</a></span>
+                        </td>
+                        <td class="action">
+                          <button class="btn btn-remove">Remove</button>
+                          <button class="btn btn-edit">Edit</button>
+                        </td>`;
+
+        row.querySelector(".btn-remove").onclick = gameRemove;
+        row.querySelector(".btn-edit").onclick = gameEdit;
+        document.querySelector("#tableGame").appendChild(row);
       }
     });
 }
 
 createTable();
+createGameTable();
 let targetId = 0;
 
 function edit(e) {
@@ -97,6 +168,32 @@ function edit(e) {
   editItem.innerHTML = editRow.innerHTML;
 }
 
+function gameEdit(e) {
+  let editItem = e.target.parentElement.parentElement;
+  targetId = editItem.querySelector(".team-name span").getAttribute("id");
+
+  let targetTeamName = editItem.querySelector(".team-name span").innerHTML;
+  let targetGameName = editItem.querySelector(".game-name span").innerHTML;
+  let targetUrl = editItem.querySelector(".gameLink a").getAttribute("href");
+
+  let editRow = document.createElement("tr");
+  editRow.classList.add("game-input");
+  editRow.innerHTML = `<td>
+                        <input id="update-team-name-input" type="text" name="${targetTeamName}" placeholder="${targetTeamName}" required="required" />
+                      </td>
+                      <td>
+                          <input id="update-game-name-input" type="text" name="${targetGameName}" placeholder="${targetGameName}" required="required"/>
+                      </td>
+                      <td>
+                          <input type="url" name="${targetUrl}" id="update-gUrl" placeholder="${targetUrl}" pattern="https://.*" size="30" required="required">
+                      </td>
+                      <td>
+                          <button id="update" class="btn btn-update" onclick="updateGameFunction()">Update</button>
+                      </td>`;
+
+  editItem.innerHTML = editRow.innerHTML;
+}
+
 function updateFunction() {
   let firstName = document.querySelector("#update-first-name-input").value;
   let lastName = document.querySelector("#update-last-name-input").value;
@@ -120,6 +217,36 @@ function updateFunction() {
     })
     .then((res) => {
       createTable();
+      location.reload();
+    });
+}
+
+function updateGameFunction() {
+  let teamName = document.querySelector("#update-team-name-input").value;
+  let gameName = document.querySelector("#update-game-name-input").value;
+  let url = document.querySelector("#update-gUrl").value;
+
+  if (!teamName) {
+    teamName = document.querySelector("#update-team-name-input").name;
+  }
+  if (!gameName) {
+    gameName = document.querySelector("#update-game-name-input").name;
+  }
+  if (!url) {
+    url = document.querySelector("#update-gUrl").name;
+  }
+
+  axios
+    .put(
+      `https://ironrest.herokuapp.com/mia-aug2020-webdevs-games/${targetId}`,
+      {
+        team_name: teamName,
+        game_name: gameName,
+        website: url,
+      }
+    )
+    .then((res) => {
+      createGameTable();
       location.reload();
     });
 }
